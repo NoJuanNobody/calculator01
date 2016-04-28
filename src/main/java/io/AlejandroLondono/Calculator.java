cd6ffc1f0afd7033ce1e540bbc3bcf2f6d3115b0
Alejandro Londono <alondono11@gmail.com>
2016-04-28 10:28:47 -0400

    commiting changes

--- src/main/java/io/AlejandroLondono/Calculator.java
+++ src/main/java/io/AlejandroLondono/Calculator.java
@@ -4,6 +4,7 @@ package io.AlejandroLondono;
  * Created by alejandrolondono on 4/27/16.
  */
 public class Calculator {
+//    this method returns the sum of two values, order does not value.
     public int sum(int value1, int value2){
         return value1+value2;
     }
@@ -13,10 +14,12 @@ public class Calculator {
         return value1 - value2;
     }
 
+//    multiply returns the product of two values. order does not matter
     public int multiply(int value1, int value2){
         return value1 * value2;
     }
 
+//    this divide function returns a quotient based on two argument values. order matters value one is divided by v2
     public int divide(int value1, int value2){
         return value1/value2;
     }
